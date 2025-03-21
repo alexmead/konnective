09e3840f4511dd1d93d4e1b33ffd1d8498541d53
Kaiden Moore <kaidenmoore@KaiMac.attlocal.net>
2025-03-21 15:25:34 -0400

    order v1

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import httpx
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from enum import Enum
 
 app = FastAPI()
@@ -10,35 +10,84 @@ KONNEKTIVE_API_URL = "https://api.konnektive.com"
 
 class PageType(Enum):
     lead = "leadPage"
-    order = "orderPage"
+    checkout = "checkoutPage"
     upsell = "upsellPage"
     thankYou = "thankYouPage"
 
-class click_params(BaseModel):
+class ClickParams(BaseModel):
     pageType: PageType
-    sessionId: Optional[str]
-    campaignId: Optional[str]
+    sessionId: Optional[str] = Field(default=None)
+    requestUri: Optional[str] = Field(default=None)
+    campaignId: Optional[str] = Field(default=None)
 
-# GET /products
-# POST /clicks
-# POST /leads
-# POST /orders
-#  - pre auth
-#  - verify data
-#  - calc sales tax & shipping
-#  - order 
-#  - if order success -> [return sucess & route upsell] || [rerun order] || [cancel order]
+class OrderParams(BaseModel):
+    firstName: str
+    lastName: str
+    address1: str
+    address2: Optional[str]
+    postalCode: str
+    city: str
+    state: str
+    country: str
+    emailAddress: str
+    phoneNumber: str
+    paySource: str
+    cardNumber: str
+    cardMonth: str
+    cardYear: str
+    cardSecurityCode: str
+    campaignId: str
+    product1_id: str
+    product1_qty: str
 
 username = "wsapi"
 password = ""
 
-@app.post("/clicks")
-async def update_clicks(params: click_params):
+@app.post("/order_v1")
+async def update_order(params: OrderParams):
     async with httpx.AsyncClient() as client:
-        if(params.pageType == PageType.lead.value):
-            response = await client.post(f"{KONNEKTIVE_API_URL}/landers/clicks/import/?loginId={username}&password={password}&pageType=leadPage&campaignId={params.campaignId}")
+        query_params = {
+            "loginId": username,
+            "password": password,
+            "firstName": params.firstName,
+            "lastName": params.lastName,
+            "address1": params.address1,
+            "postalCode": params.postalCode,
+            "city": params.city,
+            "state": params.state,
+            "country": params.country,
+            "emailAddress": params.emailAddress,
+            "phoneNumber": params.phoneNumber,
+            "shipFirstName": params.firstName,
+            "shipLastName": params.lastName,
+            "shipAddress1": params.address1,
+            "shipPostalCode": params.postalCode,
+            "shipCity": params.city,
+            "shipState": params.state,
+            "shipCountry": params.country,
+            "paySource": params.paySource,
+            "cardNumber": params.cardNumber,
+            "cardMonth": params.cardMonth,
+            "cardYear": params.cardYear,
+            "cardSecurityCode": params.cardSecurityCode,
+            "campaignId": params.campaignId,
+            "product1_id": params.product1_id,
+            "product1_qty": params.product1_qty,
+        }
+        if params.address2:
+            query_params["address2"] = params.address2
+            query_params["shipAddress2"] = params.address2
+
+        response = await client.post(f"{KONNEKTIVE_API_URL}/landers/clicks/import/", params=query_params)
+        return response.json()
+
+@app.post("/click")
+async def update_clicks(params: ClickParams):
+    async with httpx.AsyncClient() as client:
+        if(params.pageType.value == PageType.lead.value):
+            response = await client.post(f"{KONNEKTIVE_API_URL}/landers/clicks/import/?loginId={username}&password={password}&pageType=leadPage&campaignId={params.campaignId}&requestUri={params.requestUri}")
         else:
-            response = await client.post(f"{KONNEKTIVE_API_URL}/landers/clicks/import/?loginId={username}&password={password}&pageType={params.pageType}&sessionId={params.sessionId}")
+            response = await client.post(f"{KONNEKTIVE_API_URL}/landers/clicks/import/?loginId={username}&password={password}&pageType={params.pageType.value}&sessionId={params.sessionId}")
         return response.json()
 
 @app.get('/products')
